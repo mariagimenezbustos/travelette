160bd3869e414e6beaf1b79549715a89c0bc6a92
mariagimenezbustos <134734638+mariagimenezbustos@users.noreply.github.com>
2023-08-17 13:26:30 +0200

    database update

--- model/init_db.sql
+++ model/init_db.sql
@@ -11,27 +11,52 @@ SET foreign_key_checks = 1;
 --
 
 CREATE TABLE transactions(
-    id INT NOT NULL AUTO_INCREMENT,
-    date DATE not null,
-    category VARCHAR(40) not null,
-    merchant VARCHAR(100) not null,
-    description VARCHAR(255),
-    amount INT not null,
-    currency VARCHAR(40) not null,
-    PRIMARY KEY (id)
-    );
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `date` DATE NOT NULL,
+    `category_id` INT UNSIGNED NOT NULL,
+    `merchant` VARCHAR(255) NOT NULL,
+    `description` VARCHAR(255) NOT NULL,
+    `amount` INT NOT NULL,
+    `currency` VARCHAR(255) NOT NULL,
+    `user_id` INT UNSIGNED NOT NULL
+);
 
 CREATE TABLE categories(
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(40) not null,
-    PRIMARY KEY (id)
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(40) NOT NULL
 );
 
 CREATE TABLE users(
-    id INT NOT NULL AUTO_INCREMENT,
-    firstname VARCHAR(50) not null,
-    lastname VARCHAR(50) not null,
-    transactions VARCHAR(50) not null,
-    categories VARCHAR(255),
-    PRIMARY KEY (id)
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `firstname` VARCHAR(255) NOT NULL,
+    `lastname` VARCHAR(255) NOT NULL
 );
+
+ALTER TABLE
+    transactions ADD CONSTRAINT `transactions_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `users`(`id`);
+ALTER TABLE
+    transactions ADD CONSTRAINT `transactions_category_id_foreign` FOREIGN KEY(`category_id`) REFERENCES `categories`(`id`);
+
+INSERT INTO users (firstname, lastname)
+VALUES
+    ("Alexandra", "Green"),
+    ("Rafael", "Marino"),
+    ("Sofia", "Rabl"),
+    ("Chris", "Velotti");
+
+INSERT INTO categories (name)
+VALUES
+    ("food"), ("transport"), ("accomodation"), ("groceries"), ("shopping"), ("gifts"), ("personal care"), ("activities"), ("other");
+
+INSERT INTO transactions (date, merchant, description, amount, currency, category_id, user_id)
+VALUES
+    ("2023-06-01", "Vueling", "LGW - BCN flights", 600, "GBP", 2, 2),
+    ("2023-08-07", "TMB", "5-day public transport ticket", 152.80, "EUR", 2, 1),
+    ("2023-06-03", "NH Hotels", "2 Double-Rooms", 1041, "EUR", 3, 3),
+    ("2023-07-01", "Sagrada Familia", "Sagrada Familia Tickets", 100, "EUR", 8, 4),
+    ("2023-08-07", "LGW Airport", "Breakfast Service", 35.60, "GBP", 1, 1),
+    ("2023-08-07", "Mercadona", "Mercadona Bill", 263.75, "EUR", 4, 4),
+    ("2023-08-11", "Souvenirs de Barcelona", "Souvenirs", 23.96, "EUR", 6, 3),
+    ("2023-08-10", "Paella Barcelona", "Paella Barcelona Bill", 68.48, "EUR", 1, 1),
+    ("2023-08-09", "Tapas Barcelona", "Tapas Barcelona Bill", 42.20, "EUR", 1, 4),
+    ("2023-08-09", "Sangria Lifestyle", "Sangria Lifestyle Bill", 21.75, "EUR", 1, 2);
